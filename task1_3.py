ace4b75dac3081881d6789a47150eb8db11231f1
X Hu <xh297@cam.ac.uk>
2021-05-17 16:00:26 +0100

    task1.3 finished

--- task1_3.py
+++ task1_3.py
@@ -1,13 +1,13 @@
 import numpy as np
 import numpy.random as random
-import sklearn as sk
+from sklearn import linear_model
 from CartPole import *
 
 # linear model Y=CX
 
 cartpole1 = CartPole()
 
-# generating random states X and corresponding Y
+"""generating random states X and corresponding Y"""
 X = []
 Y = []
 for i in range(500):
@@ -24,6 +24,71 @@ for i in range(500):
 X = np.array(X)
 Y = np.array(Y)
 
-model = sk.linear_model.LinearRegression()
+"""performing linear regression"""
+model = linear_model.LinearRegression()
 model.fit(X,Y)
 
+"""generate model prediction"""
+Y_predict = model.predict(X)
+
+"""plotting comparison - real & predict against current state"""
+# fig, axs = plt.subplots(2,2,figsize=(12,8),constrained_layout=True)
+
+# axs[0,0].scatter(X[:,0],Y[:,0],label='real')
+# axs[0,0].scatter(X[:,0],Y_predict[:,0],label='predict')
+# axs[0,0].set_title('cart_location')
+# axs[0,0].set_xlabel('current location (m)')
+# axs[0,0].set_ylabel('next location (m)')
+
+# axs[0,1].scatter(X[:,1],Y[:,1],label='real')
+# axs[0,1].scatter(X[:,1],Y_predict[:,1],label='predict')
+# axs[0,1].set_title('cart_velocity')
+# axs[0,1].set_xlabel('current velocity (m/s)')
+# axs[0,1].set_ylabel('next velocity (m/s)')
+
+# axs[1,0].scatter(X[:,2],Y[:,2],label='real')
+# axs[1,0].scatter(X[:,2],Y_predict[:,2],label='predict')
+# axs[1,0].set_title('pole_angle')
+# axs[1,0].set_xlabel('current angle (rad)')
+# axs[1,0].set_ylabel('next angle (rad)')
+
+# axs[1,1].scatter(X[:,3],Y[:,3],label='real')
+# axs[1,1].scatter(X[:,3],Y_predict[:,3],label='predict')
+# axs[1,1].set_title('pole_velocity')
+# axs[1,1].set_xlabel('current angular velocity (rad/s)')
+# axs[1,1].set_ylabel('next angular velocity (rad/s)')
+
+# fig.suptitle('prediction vs real',fontsize=16)
+# handles, labels = axs[1,1].get_legend_handles_labels()
+# fig.legend(handles, labels, loc='middle center')
+# plt.show()
+
+"""plotting comparison - real vs predict"""
+fig, axs = plt.subplots(2,2,figsize=(12,8),constrained_layout=True)
+
+axs[0,0].scatter(Y[:,0],Y_predict[:,0])
+axs[0,0].plot(Y[:,0],Y[:,0],color='r',alpha=0.7)
+axs[0,0].set_title('cart_location')
+axs[0,0].set_xlabel('real (m)')
+axs[0,0].set_ylabel('predict (m)')
+
+axs[0,1].scatter(Y[:,1],Y_predict[:,1])
+axs[0,1].plot(Y[:,1],Y[:,1],color='r',alpha=0.7)
+axs[0,1].set_title('cart_velocity')
+axs[0,1].set_xlabel('real (m/s)')
+axs[0,1].set_ylabel('predict (m/s)')
+
+axs[1,0].scatter(Y[:,2],Y_predict[:,2])
+axs[1,0].plot(Y[:,2],Y[:,2],color='r',alpha=0.7)
+axs[1,0].set_title('pole_angle')
+axs[1,0].set_xlabel('real (rad)')
+axs[1,0].set_ylabel('predict (rad)')
+
+axs[1,1].scatter(Y[:,3],Y_predict[:,3])
+axs[1,1].plot(Y[:,3],Y[:,3],color='r',alpha=0.7)
+axs[1,1].set_title('pole_velocity')
+axs[1,1].set_xlabel('real (rad/s)')
+axs[1,1].set_ylabel('predict (rad/s)')
+
+fig.suptitle('prediction vs real',fontsize=16)
+plt.show()
